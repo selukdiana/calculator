80db70b003779cab829a46702c00a1bda66c0192
Diana Selyuk <selukdiana@gmail.com>
2025-06-15 12:35:11 +0300

    configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env) => {
+  const isDev = env.mode === "development";
+  return {
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      clean: true,
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    devServer: isDev
+      ? {
+          port: 5000,
+          open: true,
+        }
+      : undefined,
+  };
+};
